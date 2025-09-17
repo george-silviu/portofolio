4316517cee87a1bc5a10d33408f5312fb0165a43
silviucostache <costache.georgesilviu@yahoo.ro>
2025-09-17 18:43:23 +0300

    starting nav bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,13 @@ import "./Header.scss";
 
 export const Header: React.FC = () => {
     return <header className="header">
-        Logo
+        <a className="logo">Logo</a>
+        <div className="nav-right">
+            <a className="nav-item">About</a>
+            <a className="nav-item">Skills</a>
+            <a className="nav-item">Projects</a>
+            <a className="nav-item">Blog</a>
+            <a className="nav-item">Contact</a>
+        </div>
     </header>
 }
