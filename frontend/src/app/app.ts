bca8c54fe63a03758874af1af604631984a9aa74
george-silviu <costache.georgesilviu@yahoo.ro>
2026-07-15 23:00:28 +0300

    starting to build the main layout

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Dashboard } from './components/dashboard/dashboad.component';
+import { Navbar } from './components/navbar/navbar.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Dashboard],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('frontend');
