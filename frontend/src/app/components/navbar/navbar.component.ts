1b878e90996719cb743c697d3a60b0ff08166696
george-silviu <costache.georgesilviu@yahoo.ro>
2026-08-03 23:14:48 +0300

    added 'selected' state styles for navlinks

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,20 @@
 import { Component } from "@angular/core";
+import { ObjectsColumn } from '@primeicons/angular/objects-column';
+import { FolderOpen } from '@primeicons/angular/folder-open';
+import { FileEdit } from '@primeicons/angular/file-edit';
+import { Bookmark } from '@primeicons/angular/bookmark';
 
 @Component({
+    standalone: true,
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
-    styleUrl: './navbar.component.scss'
+    styleUrl: './navbar.component.scss',
+    imports: [
+        ObjectsColumn,
+        FolderOpen,
+        FileEdit,
+        Bookmark
+    ]
 })
 export class Navbar {
 
